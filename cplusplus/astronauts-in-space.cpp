f7fb494b65b144c031669a4ea575f9bf481b96c9
David Siegel <dasiege@microsoft.com>
2018-02-04 12:47:56 -0800

    update (quicktype 8.5.7)

--- cplusplus/astronauts-in-space.cpp
+++ cplusplus/astronauts-in-space.cpp
@@ -5,35 +5,12 @@
 //
 //  Then include this file, and then do
 //
-//     AstronautsInSpace data = nlohmann::json::parse(jsonString);
-//     IssCurrentLocation data = nlohmann::json::parse(jsonString);
 
 #include "json.hpp"
 
 namespace quicktype {
     using nlohmann::json;
 
-    struct Person {
-        std::string craft;
-        std::string name;
-    };
-
-    struct AstronautsInSpace {
-        int64_t number;
-        std::vector<struct Person> people;
-        std::string message;
-    };
-
-    struct IssPosition {
-        std::string longitude;
-        std::string latitude;
-    };
-
-    struct IssCurrentLocation {
-        int64_t timestamp;
-        struct IssPosition iss_position;
-        std::string message;
-    };
     
     inline json get_untyped(const json &j, const char *property) {
         if (j.find(property) != j.end()) {
@@ -44,52 +21,4 @@ namespace quicktype {
 }
 
 namespace nlohmann {
-
-    inline void from_json(const json& _j, struct quicktype::Person& _x) {
-        _x.craft = _j.at("craft").get<std::string>();
-        _x.name = _j.at("name").get<std::string>();
-    }
-
-    inline void to_json(json& _j, const struct quicktype::Person& _x) {
-        _j = json::object();
-        _j["craft"] = _x.craft;
-        _j["name"] = _x.name;
-    }
-
-    inline void from_json(const json& _j, struct quicktype::AstronautsInSpace& _x) {
-        _x.number = _j.at("number").get<int64_t>();
-        _x.people = _j.at("people").get<std::vector<struct quicktype::Person>>();
-        _x.message = _j.at("message").get<std::string>();
-    }
-
-    inline void to_json(json& _j, const struct quicktype::AstronautsInSpace& _x) {
-        _j = json::object();
-        _j["number"] = _x.number;
-        _j["people"] = _x.people;
-        _j["message"] = _x.message;
-    }
-
-    inline void from_json(const json& _j, struct quicktype::IssPosition& _x) {
-        _x.longitude = _j.at("longitude").get<std::string>();
-        _x.latitude = _j.at("latitude").get<std::string>();
-    }
-
-    inline void to_json(json& _j, const struct quicktype::IssPosition& _x) {
-        _j = json::object();
-        _j["longitude"] = _x.longitude;
-        _j["latitude"] = _x.latitude;
-    }
-
-    inline void from_json(const json& _j, struct quicktype::IssCurrentLocation& _x) {
-        _x.timestamp = _j.at("timestamp").get<int64_t>();
-        _x.iss_position = _j.at("iss_position").get<struct quicktype::IssPosition>();
-        _x.message = _j.at("message").get<std::string>();
-    }
-
-    inline void to_json(json& _j, const struct quicktype::IssCurrentLocation& _x) {
-        _j = json::object();
-        _j["timestamp"] = _x.timestamp;
-        _j["iss_position"] = _x.iss_position;
-        _j["message"] = _x.message;
-    }
 }
--- elm/astronauts-in-space.elm
+++ elm/astronauts-in-space.elm
@@ -5,22 +5,12 @@
 -- add these imports
 --
 --     import Json.Decode exposing (decodeString)`);
---     import QuickType exposing (astronautsInSpace, issCurrentLocation)
+--     import QuickType exposing ()
 --
 -- and you're off to the races with
 --
---     decodeString astronautsInSpace myJsonString
---     decodeString issCurrentLocation myJsonString
 
 module QuickType exposing
-    ( AstronautsInSpace
-    , astronautsInSpaceToString
-    , astronautsInSpace
-    , ISSCurrentLocation
-    , issCurrentLocationToString
-    , issCurrentLocation
-    , Person
-    , IssPosition
     )
 
 import Json.Decode as Jdec
@@ -29,91 +19,7 @@ import Json.Encode as Jenc
 import Dict exposing (Dict, map, toList)
 import Array exposing (Array, map)
 
-type alias AstronautsInSpace =
-    { number : Int
-    , people : Array Person
-    , message : String
-    }
-
-type alias Person =
-    { craft : String
-    , name : String
-    }
-
-type alias ISSCurrentLocation =
-    { timestamp : Int
-    , issPosition : IssPosition
-    , message : String
-    }
-
-type alias IssPosition =
-    { longitude : String
-    , latitude : String
-    }
-
 -- decoders and encoders
-
-astronautsInSpaceToString : AstronautsInSpace -> String
-astronautsInSpaceToString r = Jenc.encode 0 (encodeAstronautsInSpace r)
-
-issCurrentLocationToString : ISSCurrentLocation -> String
-issCurrentLocationToString r = Jenc.encode 0 (encodeISSCurrentLocation r)
-
-astronautsInSpace : Jdec.Decoder AstronautsInSpace
-astronautsInSpace =
-    Jpipe.decode AstronautsInSpace
-        |> Jpipe.required "number" Jdec.int
-        |> Jpipe.required "people" (Jdec.array person)
-        |> Jpipe.required "message" Jdec.string
-
-encodeAstronautsInSpace : AstronautsInSpace -> Jenc.Value
-encodeAstronautsInSpace x =
-    Jenc.object
-        [ ("number", Jenc.int x.number)
-        , ("people", makeArrayEncoder encodePerson x.people)
-        , ("message", Jenc.string x.message)
-        ]
-
-person : Jdec.Decoder Person
-person =
-    Jpipe.decode Person
-        |> Jpipe.required "craft" Jdec.string
-        |> Jpipe.required "name" Jdec.string
-
-encodePerson : Person -> Jenc.Value
-encodePerson x =
-    Jenc.object
-        [ ("craft", Jenc.string x.craft)
-        , ("name", Jenc.string x.name)
-        ]
-
-issCurrentLocation : Jdec.Decoder ISSCurrentLocation
-issCurrentLocation =
-    Jpipe.decode ISSCurrentLocation
-        |> Jpipe.required "timestamp" Jdec.int
-        |> Jpipe.required "iss_position" issPosition
-        |> Jpipe.required "message" Jdec.string
-
-encodeISSCurrentLocation : ISSCurrentLocation -> Jenc.Value
-encodeISSCurrentLocation x =
-    Jenc.object
-        [ ("timestamp", Jenc.int x.timestamp)
-        , ("iss_position", encodeIssPosition x.issPosition)
-        , ("message", Jenc.string x.message)
-        ]
-
-issPosition : Jdec.Decoder IssPosition
-issPosition =
-    Jpipe.decode IssPosition
-        |> Jpipe.required "longitude" Jdec.string
-        |> Jpipe.required "latitude" Jdec.string
-
-encodeIssPosition : IssPosition -> Jenc.Value
-encodeIssPosition x =
-    Jenc.object
-        [ ("longitude", Jenc.string x.longitude)
-        , ("latitude", Jenc.string x.latitude)
-        ]
 
 --- encoder helpers
 
